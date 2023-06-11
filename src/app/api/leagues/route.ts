f98e9285e4eefa000700fea57631efa6a71e0a7f
sindre-kodehode <sindre.kodehode@gmail.com>
2023-06-11 19:23:14 +0200

    add comment sections and format code

--- src/app/api/leagues/route.ts
+++ src/app/api/leagues/route.ts
@@ -1,77 +1,95 @@
 //******************************************************************************
 // imports
 //******************************************************************************
-import { League, PrismaClient } from '@prisma/client';
-import { NextResponse, NextRequest } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+import { League       } from '@prisma/client';
+import { NextResponse } from 'next/server';
+import { NextRequest  } from 'next/server';
+
 
 //******************************************************************************
 // server
 //******************************************************************************
 const prisma = new PrismaClient();
 
+
+//******************************************************************************
+// GET
+//******************************************************************************
 export async function GET() {
     const leagues = await prisma.league.findMany();
 
     return NextResponse.json({
-        success: true,
-        data: leagues,
+        success : true    ,
+        data    : leagues ,
     });
 }
 
-export async function POST(request: NextRequest) {
-    const { name }: League = await request.json();
 
-    if (!name) return NextResponse.json({
-        success: false,
-        message: "'name' not given.",
+//******************************************************************************
+// POST
+//******************************************************************************
+export async function POST( request : NextRequest ) {
+    const { name } : League = await request.json();
+
+    if ( !name ) return NextResponse.json({
+        success : false               ,
+        message : "'name' not given." ,
     })
 
     const newLeague = await prisma.league.create({
-        data: { name }
+        data : { name }
     });
 
     return NextResponse.json({
-        success: true,
-        data: newLeague,
+        success : true      ,
+        data    : newLeague ,
     });
 }
 
-export async function PUT(request: NextRequest) {
-    const { id, name }: League = await request.json();
 
-    if (!name || !id) return NextResponse.json({
-        success: false,
-        message: "missing required data",
+//******************************************************************************
+// PUT
+//******************************************************************************
+export async function PUT( request : NextRequest ) {
+    const { id, name } : League = await request.json();
+
+    if ( !name || !id ) return NextResponse.json({
+        success : false                   ,
+        message : "missing required data" ,
     })
 
     const updatedLeague = await prisma.league.update({
-        where: { id },
-        data: { name }
+        where : { id   } ,
+        data  : { name } ,
     });
 
     return NextResponse.json({
-        success: true,
-        data: updatedLeague,
+        success : true          ,
+        data    : updatedLeague ,
     });
 }
 
-export async function DELETE(request: NextRequest) {
-    const data: League = await request.json();
 
-    const { id } = data;
-    console.log(data, id);
+//******************************************************************************
+// DELETE
+// does not currently work on localhost for next@13.4.5
+// ISSUE: https://github.com/vercel/next.js/issues/48096
+//******************************************************************************
+export async function DELETE( request : NextRequest ) {
+    const { id } : League = await request.json();
 
-    if (!id) return NextResponse.json({
-        success: false,
-        message: "missing required data",
+    if ( !id ) return NextResponse.json({
+        success : false                   ,
+        message : "missing required data" ,
     })
 
     const deletedLeague = await prisma.league.delete({
-        where: { id },
+        where : { id } ,
     });
 
     return NextResponse.json({
-        success: true,
-        data: deletedLeague,
+        success : true          ,
+        data    : deletedLeague ,
     });
 }
